f1c1309f9581f34f1a9638133d8d405bd347c57f
Khaled Oweas <khaled.oweas@yahoo.com>
2023-02-18 01:03:33 +0200

    Update main.js
    Co-Authored-By: egabikoweas <125705412+egabikoweas@users.noreply.github.com>

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 // This is First And Good Coode 
 console.log("hello");
 
+
+// Onther dev 
 // this gives me issues 
 // should be log not write line 
 // if you need anything please call me @khaledoweas
